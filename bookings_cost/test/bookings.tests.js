6a6dd6d1933b754740997fad68960952421ba93a
Zeenat <zeenatavontuur@gmail.com>
2022-04-22 15:34:52 +0200

    Uploading changes

--- bookings_cost/test/bookings.tests.js
+++ bookings_cost/test/bookings.tests.js
@@ -1,1 +1,10 @@
+describe("funtion should take in a booking requests to Kamva's Cattery",function(){
 
+it(Should cost 'R135.00' when called with check_in_day: 'Monday',duration: 3,cat_name : 'Snowy', function(){
+
+assert.equal('R135.00',bookingCost('Monday'))
+
+})
+
+
+})
